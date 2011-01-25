264b5577134f25bec902de7281ec16690a84da82
Corbin Simpson <simpsoco@osuosl.org>
2011-01-25 13:35:24 -0800

    site: More wiring.
    Now we're in VNC-land.

--- site.py
+++ site.py
@@ -1,3 +1,8 @@
+from functools import partial
+
+from twisted.web.resource import Resource
+from twisted.python import log
+
 from vncap.protocol import VNCServerAuthenticator
 from vncap.websocket import WebSocketSite, WebSocketHandler
 
@@ -12,10 +17,11 @@ class VNCHandler(WebSocketHandler):
     A handler that pretends the other side of the connection is VNC over WS.
     """
 
-    def __init__(self, password):
+    def __init__(self, transport, password=""):
         self.password = password
 
     def connectionMade(self):
+        log.msg("Handling request")
         self.protocol = VNCServerAuthenticator(self.password)
         self.protocol.transport = DummyTransport()
         self.protocol.connectionMade()
@@ -30,5 +36,7 @@ class VNCHandler(WebSocketHandler):
 class VNCSite(WebSocketSite):
 
     def __init__(self, password):
-        handler = VNCHandler(password)
-        WebSocketSite.__init__(self, handler)
+        handler = partial(VNCHandler, password=password)
+        resource = Resource()
+        WebSocketSite.__init__(self, resource)
+        self.addHandler("/", handler)
