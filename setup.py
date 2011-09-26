346df033fafd61f98d6ef14f367d946a9fb3252d
Corbin Simpson <simpsoco@osuosl.org>
2011-09-26 16:24:19 -0700

    Version 0.6, now with README!

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import setup
 
 setup(
     name="txWS",
-    version="0.5",
-    py_modules = ["txws"],
+    version="0.6",
+    py_modules=["txws"],
     install_requires=[
         "Twisted",
     ],
     author="Corbin Simpson",
     author_email="simpsoco@osuosl.org",
     description="Twisted WebSockets wrapper",
+    long_description=open("README.rst").read(),
     license="MIT/X11",
     url="http://github.com/MostAwesomeDude/txWS",
 )
