2c96ef69dec9387009c116c7bca7463d3f82bc40
Corbin Simpson <simpsoco@osuosl.org>
2011-09-28 13:50:42 -0700

    Add changelog, bump version.

---
+++ CHANGELOG.rst
@@ -0,0 +1,14 @@
+0.6.1
+=====
+
+* Fix issue with installation from PyPI
+
+0.6
+===
+
+* Start of availability on PyPI
+
+0.5
+===
+
+* Initial release
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="txWS",
-    version="0.6",
+    version="0.6.1",
     py_modules=["txws"],
     install_requires=[
         "Twisted",
